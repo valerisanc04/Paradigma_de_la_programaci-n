e84a51a7362601b7a5dc30b78cb183d436463241
valerisanc04 <valeri.sanc04@gmail.com>
2026-02-25 16:03:27 -0500

    suma y resta

---
+++ Programacion_secuencial.js
@@ -0,0 +1,19 @@
+let nombre = "valeri";
+let edad = 15;
+let apellido = "sanchez";
+let numero = 7;
+function sumar(a, b) {
+    return a + b;
+
+}
+function restar(a, b) {
+    let operacion = a - b;
+    return operacion;
+}
+
+
+console.log("el resultado de la suma es: " + sumar(numero, edad))
+let resultadoresta = restar(5, 7);
+
+console.log("resultado de la resta: " + resultadoresta);
+console.log ("nombre completo:" + nombre + " " + apellido + ", edad :" + edad);
